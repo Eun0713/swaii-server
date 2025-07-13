6d8857f96f55afbd008442f7e3b975fc5c702022
Eun0713 <daeunlee0713@naver.com>
2025-07-13 23:16:30 +0900

    feat: 제스처 저장 요청 처리 함수 구현

---
+++ src/controllers/gestureController.js
@@ -0,0 +1,26 @@
+import { saveGesture } from "../services/gestureService.js";
+import { HTTP_STATUS } from "../constants/httpStatus.js";
+import { MESSAGES } from "../constants/messages.js";
+
+export const saveGestureController = async (req, res) => {
+  const { email, gesture } = req.body;
+
+  if (!email || !gesture) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: MESSAGES.MISSING_DATA });
+  }
+
+  try {
+    const savedGesture = await saveGesture(email, gesture);
+    res.status(HTTP_STATUS.CREATED).json({
+      message: MESSAGES.GESTURE_SAVE_SUCCESS,
+      data: savedGesture,
+    });
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: MESSAGES.GESTURE_SAVE_FAIL,
+      error: error.message,
+    });
+  }
+};
