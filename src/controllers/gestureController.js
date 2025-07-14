b525a00e2eb13d265c24d0e7ab73a106b7dfd6a5
Eun0713 <daeunlee0713@naver.com>
2025-07-14 16:42:13 +0900

    feat: GET /gestures 컨트롤러 및 라우터 연결

--- src/controllers/gestureController.js
+++ src/controllers/gestureController.js
@@ -1,4 +1,4 @@
-import { saveGesture } from "../services/gestureService.js";
+import { saveGesture, getGesturesByEmail } from "../services/gestureService.js";
 import { HTTP_STATUS } from "../constants/httpStatus.js";
 import { MESSAGES } from "../constants/messages.js";
 
@@ -22,5 +22,26 @@ export const saveGestureController = async (req, res) => {
       message: MESSAGES.GESTURE_SAVE_FAIL,
       error: error.message,
     });
+  }
+};
+
+export const getGesturesController = async (req, res) => {
+  const { email } = req.query;
+
+  if (!email) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: MESSAGES.MISSING_DATA });
+  }
+
+  try {
+    const gestures = await getGesturesByEmail(email);
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ message: MESSAGES.GESTURE_FETCH_SUCCESS, data: gestures });
+  } catch (error) {
+    res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .json({ message: MESSAGES.GESTURE_FETCH_FAIL, error: error.message });
   }
 };
--- src/routes/gestureRoutes.js
+++ src/routes/gestureRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { saveGestureController } from "../controllers/gestureController.js";
+import {
+  saveGestureController,
+  getGesturesController,
+} from "../controllers/gestureController.js";
 
 const router = express.Router();
 
 router.post("/", saveGestureController);
+router.get("/", getGesturesController);
 
 export default router;
