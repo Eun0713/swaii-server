0cf3b0d9dfec202720846b3b97c9a14a9723e009
Eun0713 <daeunlee0713@naver.com>
2025-07-14 16:42:42 +0900

    chore: 제스처 조회 관련 메시지 상수 추가
    - GESTURE_FETCH_SUCCESS, GESTURE_FETCH_FAIL 메시지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,5 +1,7 @@
 export const MESSAGES = {
   GESTURE_SAVE_SUCCESS: "제스처 저장 완료",
   GESTURE_SAVE_FAIL: "제스처 저장 실패",
+  GESTURE_FETCH_SUCCESS: "제스처 조회 완료",
+  GESTURE_FETCH_FAIL: "제스처 조회 실패",
   MISSING_DATA: "이메일과 제스처 데이터가 필요합니다.",
 };
