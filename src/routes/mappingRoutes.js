504b63fbdb0458e14d0d0479783c5d9f122e7081
Eun0713 <daeunlee0713@naver.com>
2025-07-15 21:56:26 +0900

    feat: DELETE /mappings 엔드포인트 추가

--- src/routes/mappingRoutes.js
+++ src/routes/mappingRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   saveMappingController,
   getMappingsController,
+  deleteMappingController,
 } from "../controllers/mappingController.js";
 
 const router = express.Router();
 
 router.post("/", saveMappingController);
 router.get("/", getMappingsController);
+router.delete("/", deleteMappingController);
 
 export default router;
