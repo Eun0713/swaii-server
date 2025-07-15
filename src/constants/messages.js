afe02f6c15d8382b4c295588c24f2a8e9ba840b8
Eun0713 <daeunlee0713@naver.com>
2025-07-15 17:25:59 +0900

    feat: 제스처 삭제 관련 메시지 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,4 +4,7 @@ export const MESSAGES = {
   GESTURE_FETCH_SUCCESS: "제스처 조회 완료",
   GESTURE_FETCH_FAIL: "제스처 조회 실패",
   MISSING_DATA: "이메일과 제스처 데이터가 필요합니다.",
+  INVALID_REQUEST: "잘못된 요청입니다.",
+  DELETE_SUCCESS: "제스처 삭제 완료",
+  DELETE_FAIL: "제스처 삭제 실패",
 };
