f84ea21b382c37383ddbbfa2f1c6dbf998be598c
Eun0713 <daeunlee0713@naver.com>
2025-07-15 17:26:30 +0900

    feat: DELETE /gestures 라우터 연결

--- src/routes/gestureRoutes.js
+++ src/routes/gestureRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   saveGestureController,
   getGesturesController,
+  deleteGestureController,
 } from "../controllers/gestureController.js";
 
 const router = express.Router();
 
 router.post("/", saveGestureController);
 router.get("/", getGesturesController);
+router.delete("/", deleteGestureController);
 
 export default router;
