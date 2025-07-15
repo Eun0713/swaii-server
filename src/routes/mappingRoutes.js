afcf273fd0448b4ffa6bbc544bb3a4a5b3a0a36c
Eun0713 <daeunlee0713@naver.com>
2025-07-15 19:41:00 +0900

    feat: GET /mappings 라우터 설정

--- src/routes/mappingRoutes.js
+++ src/routes/mappingRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { saveMappingController } from "../controllers/mappingController.js";
+import {
+  saveMappingController,
+  getMappingsController,
+} from "../controllers/mappingController.js";
 
 const router = express.Router();
 
 router.post("/", saveMappingController);
+router.get("/", getMappingsController);
 
 export default router;
